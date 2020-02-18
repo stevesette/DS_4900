0e46bb539915d9f76ce825cdfbdb6f59bec3471f
stevesette <setteducati.s@husky.neu.edu>
2020-02-18 18:09:30 -0500

    added research papers, spark starters, and gitignore

---
+++ learning_spark/tf_idf_twitter.py
@@ -0,0 +1,129 @@
+import findspark
+findspark.init()
+import pyspark as ps
+import warnings
+from pyspark.sql import SQLContext
+
+try:
+    # create SparkContext on all CPUs available: in my case I have 4 CPUs on my laptop
+    sc = ps.SparkContext('local[4]')
+    sqlContext = SQLContext(sc)
+    print("Just created a SparkContext")
+except ValueError:
+    warnings.warn("SparkContext already exists in this scope")
+
+
+df = sqlContext.read.format('com.databricks.spark.csv').options(header='false').load('sentiment140dataset/training.1600000.processed.noemoticon.csv')
+type(df)
+print(df.show(5))
+
+df = df.dropna()
+print(df.count())
+
+(train_set, val_set, test_set) = df.randomSplit([0.98, 0.01, 0.01], seed = 2000)
+
+
+
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline
+
+tokenizer = Tokenizer(inputCol="_c5", outputCol="words")
+hashtf = HashingTF(numFeatures=2**16, inputCol="words", outputCol='tf')
+idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
+label_stringIdx = StringIndexer(inputCol = "_c0", outputCol = "label")
+pipeline = Pipeline(stages=[tokenizer, hashtf, idf, label_stringIdx])
+
+pipelineFit = pipeline.fit(train_set)
+train_df = pipelineFit.transform(train_set)
+val_df = pipelineFit.transform(val_set)
+train_df.show(5)
+
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression(maxIter=100)
+lrModel = lr.fit(train_df)
+predictions = lrModel.transform(val_df)
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
+evaluator.evaluate(predictions)
+
+accuracy = predictions.filter(predictions.label == predictions.prediction).count() / float(val_set.count())
+print(accuracy)
+
+
+from pyspark.ml.feature import CountVectorizer
+
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+cv = CountVectorizer(vocabSize=2**16, inputCol="words", outputCol='cv')
+idf = IDF(inputCol='cv', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
+label_stringIdx = StringIndexer(inputCol = "target", outputCol = "label")
+lr = LogisticRegression(maxIter=100)
+pipeline = Pipeline(stages=[tokenizer, cv, idf, label_stringIdx, lr])
+
+pipelineFit = pipeline.fit(train_set)
+predictions = pipelineFit.transform(val_set)
+accuracy = predictions.filter(predictions.label == predictions.prediction).count() / float(val_set.count())
+roc_auc = evaluator.evaluate(predictions)
+
+print("Accuracy Score: {0:.4f}".format(accuracy))
+print("ROC-AUC: {0:.4f}".format(roc_auc))
+
+from pyspark.ml.feature import NGram, VectorAssembler
+from pyspark.ml.feature import ChiSqSelector
+
+def build_trigrams(inputCol=["text","target"], n=3):
+    tokenizer = [Tokenizer(inputCol="text", outputCol="words")]
+    ngrams = [
+        NGram(n=i, inputCol="words", outputCol="{0}_grams".format(i))
+        for i in range(1, n + 1)
+    ]
+
+    cv = [
+        CountVectorizer(vocabSize=2**14,inputCol="{0}_grams".format(i),
+            outputCol="{0}_tf".format(i))
+        for i in range(1, n + 1)
+    ]
+    idf = [IDF(inputCol="{0}_tf".format(i), outputCol="{0}_tfidf".format(i), minDocFreq=5) for i in range(1, n + 1)]
+
+    assembler = [VectorAssembler(
+        inputCols=["{0}_tfidf".format(i) for i in range(1, n + 1)],
+        outputCol="rawFeatures"
+    )]
+    label_stringIdx = [StringIndexer(inputCol = "target", outputCol = "label")]
+    selector = [ChiSqSelector(numTopFeatures=2**14,featuresCol='rawFeatures', outputCol="features")]
+    lr = [LogisticRegression(maxIter=100)]
+    return Pipeline(stages=tokenizer + ngrams + cv + idf+ assembler + label_stringIdx+selector+lr)
+
+trigram_pipelineFit = build_trigrams().fit(train_set)
+predictions = trigram_pipelineFit.transform(val_set)
+accuracy = predictions.filter(predictions.label == predictions.prediction).count() / float(dev_set.count())
+roc_auc = evaluator.evaluate(predictions)
+# print accuracy, roc_auc
+print("Accuracy Score: {0:.4f}".format(accuracy))
+print("ROC-AUC: {0:.4f}".format(roc_auc))
+
+def build_ngrams_wocs(inputCol=["text","target"], n=3):
+    tokenizer = [Tokenizer(inputCol="text", outputCol="words")]
+    ngrams = [
+        NGram(n=i, inputCol="words", outputCol="{0}_grams".format(i))
+        for i in range(1, n + 1)
+    ]
+
+    cv = [
+        CountVectorizer(vocabSize=5460,inputCol="{0}_grams".format(i),
+            outputCol="{0}_tf".format(i))
+        for i in range(1, n + 1)
+    ]
+    idf = [IDF(inputCol="{0}_tf".format(i), outputCol="{0}_tfidf".format(i), minDocFreq=5) for i in range(1, n + 1)]
+
+    assembler = [VectorAssembler(
+        inputCols=["{0}_tfidf".format(i) for i in range(1, n + 1)],
+        outputCol="features"
+    )]
+    label_stringIdx = [StringIndexer(inputCol = "target", outputCol = "label")]
+    lr = [LogisticRegression(maxIter=100)]
+    return Pipeline(stages=tokenizer + ngrams + cv + idf+ assembler + label_stringIdx+lr)
+
+
+
+
